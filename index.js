fdb2b3ab11a7891e6c8b1f528c1a5440dcc7726c
Bliss <blissmaake140@gmail.com>
2022-11-25 15:27:11 +0300

    Done Assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (arr) => [arr[0], arr[1]];
+// // const returnFirstTwoDrivers = function() {
+// // return [arr[0], arr[1]]
+// } 
+ 
+
+const returnLastTwoDrivers = (arr) => arr.slice(-2);
+// // const returnLastTwoDrivers = function (){
+//     return [arr[0].length-1, arr[1].length -2]
+// }
+
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier){
+    return function (fare) {
+        return fare * multiplier;
+    }
+}
+
+function fareDoubler (fare) {
+    return fare * 2;
+}
+
+function fareTripler (fare) {
+    return fare * 3;
+
+}
+
+function selectDifferentDrivers (arrdrivers, firstorlast) {
+    return firstorlast(arrdrivers)
+
+}
+// firstorlast - First 2 drivers & Last Two drivers
+// arr
